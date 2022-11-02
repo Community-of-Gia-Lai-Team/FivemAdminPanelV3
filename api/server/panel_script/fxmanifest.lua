81c658911d010b35c80e23db71f4700ee24045e6
unknown <dani.dcshop.gg@gmail.com>
2022-11-02 22:20:55 +0100

    Update server script

--- api/server/panel_script/fxmanifest.lua
+++ api/server/panel_script/fxmanifest.lua
@@ -1,2 +1,6 @@
+fx_version 'adamant'
+
+game 'gta5'
+
 server_script "server/server.js"
 client_script "client/client.js"
--- api/server/panel_script/server/server.js
+++ api/server/panel_script/server/server.js
@@ -1,5 +1,36 @@
-const WebSocket = require('ws');
-const ws = new WebSocket.Server({port: 8000});
-ws.on('connection', function connection(wss){
-    console.log(`New connection from ${wss}`);
-})
+const WebSocketClient = require('websocket').client;
+
+var client = new WebSocketClient();
+
+client.on('connectFailed', function(error) {
+    console.log('Connect Error: ' + error.toString());
+});
+
+client.on('connect', function(connection) {
+    console.log('Connection established to Admin Panel!');
+    
+    connection.on('error', function(error) {
+        console.log("Connection error: " + error.toString());
+    });
+    
+    connection.on('close', function() {
+        console.log('Connection closed!');
+    });
+    
+    connection.on('message', function(message) {
+        const data = message.utf8Data.split(' ');
+        switch(data[0]){
+            case 'screenshot':
+                console.log(`Get screenshot of ${data[1]} && ${data[2]}`)
+                for(let i = 0; i < 200; i++){
+                    if(GetPlayerName(`${i}`) == data[2]){
+                        connection.sendUTF(`ID: ${i}`);
+                    }
+                }
+                //connection.message("Screenshot taked");
+                //connection.sendUTF("url de la foto");
+        }
+    });
+});
+
+client.connect('ws://localhost:1350/');
